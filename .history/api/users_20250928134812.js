014d5d2de589d9d3f2aab28bcd920c5735291913
renzoSalosagcol <renzosalosagcol@csu.fullerton.edu>
2025-09-28 13:48:32 -0700

    Database Connection Added

---
+++ .history/api/users_20250928134812.js
@@ -0,0 +1,15 @@
+import { connect } from "../lib/db.js";
+
+async function fetchUsers() {
+  try {
+    const connection = await connect();
+    const [rows] = await connection.execute('SELECT * FROM users');
+    await connection.end();
+    return rows;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
+}
+
+fetchUsers()
